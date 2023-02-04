079a03cef629a25f6991bf8a0106c596a0471731
snberanek <36345444+snberanek@users.noreply.github.com>
2023-02-04 17:40:34 -0600

    approaching HIM will cause his theme to play

---
+++ roots/Assets/Scripts/him.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class him : MonoBehaviour
+{
+
+    public AudioSource song;
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            song.Play();
+        }
+    }
+
+}
