b38ed237e0c52232e970d691d0a260ef54134fcc
snberanek <36345444+snberanek@users.noreply.github.com>
2023-02-04 22:40:32 -0600

    RotateAround am I right?

---
+++ roots/Assets/Scripts/orbit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbit : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private float degreesPerSecond = 45;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, Vector3.forward, degreesPerSecond * Time.deltaTime);
+    }
+}
